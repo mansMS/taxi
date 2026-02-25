07ffb97fbf438412daa4328e33f85299353552a6
mansms <47422885+mansMS@users.noreply.github.com>
2026-02-26 00:16:34 +0300

    chore: versel deploy configurate

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,10 +5,10 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
+  // migrations: {
+  //   path: "prisma/migrations",
+  // },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env["DIRECT_DATABASE_URL"],
   },
 });
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter }).$extends(withAccelerate());
 
 export { prisma };
