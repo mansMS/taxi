fe0259c0ef85eccbbcb93d83523339ce1d54cccf
mansms <47422885+mansMS@users.noreply.github.com>
2026-02-28 10:53:36 +0300

    fix: correct accelerate connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,13 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter }).$extends(withAccelerate());
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter }).$extends(withAccelerate());
 
-// const prisma = new PrismaClient({ accelerateUrl: connectionString }).$extends(withAccelerate());
+const prisma = new PrismaClient({ accelerateUrl: connectionString }).$extends(withAccelerate());
 
 export { prisma };
